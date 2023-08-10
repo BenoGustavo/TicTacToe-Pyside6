ed79037cd2301ecc9858f4513b5ab0e09271a3cb
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-08-10 12:55:28 -0300

    Added plater labels and Player simbols

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-#Other imports
+# Other imports
 import sys
 
-#PySide Imports
+# PySide Imports
 from PySide6.QtWidgets import QApplication
 
-#My Imports
+# My Imports
+from player_names import playerLabels, playerSimbol
 from main_window import MainWindow
 from tittle import tittleLabel
 
 if __name__ == "__main__":
-
-    #Creating the app
+    # Creating the app
     app = QApplication(sys.argv)
-    window = MainWindow(app=app)
+    window = MainWindow()
 
-    #Adding the tittle to the Layout
-    tittleWidget = tittleLabel()
+    # Adding the tittle to the Layout
+    tittleWidget = tittleLabel("Tic-Tac-Toe")
     window.addWidgetToVLayout(tittleWidget)
 
+    playerSimbols = playerSimbol()
+    window.vLayout.addLayout(playerSimbols)
+
+    playerSimbols._insertOnSimbolsOnWindow()
+
+    playerLabel = playerLabels()
+    window.vLayout.addLayout(playerLabel)
+
+    playerLabel._insertLabelOnScreen()
+
+    # Remove the permission to resize the window
+    window.AdjustFixedSize()
+
+    # Execute the app
     window.show()
     app.exec()
