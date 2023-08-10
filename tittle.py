93530bfe37970d3114e661febb265760e4457484
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-08-10 12:58:04 -0300

    some minor changes to the design

--- tittle.py
+++ tittle.py
@@ -1,16 +1,21 @@
-#Imports from PySide
+# Imports from PySide
 from PySide6.QtWidgets import QLabel
 from PySide6.QtCore import Qt
 
-#My Imports
+# My Imports
 from styles import BIG_FONT_SIZE
 
+
 class tittleLabel(QLabel):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.configTittleStyle()
 
     def configTittleStyle(self):
-        self.setStyleSheet(f"font-size:{BIG_FONT_SIZE}px")
-        self.setAlignment(Qt.AlignmentFlag.AlignHCenter,Qt.AlignmentFlag.AlignTop)
+        self.setStyleSheet(f"font-size:{BIG_FONT_SIZE}px; font-weight:bold;")
+
+        # Setting up the padding
+        self.setContentsMargins(130, 15, 130, 10)  # (left, top, right, bottom)
+
+        self.setAlignment(Qt.AlignmentFlag.AlignHCenter)
