a59f97ed9a277fd449224715a0f4c20e70078467
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-08-10 12:56:35 -0300

    fix some minor problems to run the program

--- main_window.py
+++ main_window.py
@@ -1,31 +1,32 @@
-#My Imports
+# My Imports
 
-#Imports from QT
+# Imports from QT
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QMainWindow,QWidget ,QVBoxLayout
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
 
-        #Window Tittle
+        # Window Tittle
         self.setWindowTitle("Tic-Tac-Toe")
 
-        #Basic Layout
+        # Basic Layout
         self.centralWidget = QWidget()
         self.vLayout = QVBoxLayout()
         self.centralWidget.setLayout(self.vLayout)
         self.setCentralWidget(self.centralWidget)
-        
-        #Setting up icons
-        #self.setWindowIcon(Later:put icon path here)
-        #app.setWindowIcon(Later:put icon path here)
+
+        # Setting up icons
+        # self.setWindowIcon(Later:put icon path here)
+        # app.setWindowIcon(Later:put icon path here)
 
     def AdjustFixedSize(self):
-        #NEEDS TO BE THE LAST THING SETTED 
+        # NEEDS TO BE THE LAST THING SETTED
         self.adjustSize()
         self.setFixedSize(self.width(), self.height())
 
-    def addWidgetToVLayout(self,widget: QWidget):
+    def addWidgetToVLayout(self, widget: QWidget):
         self.vLayout.addWidget(widget)
