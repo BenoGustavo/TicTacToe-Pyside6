43b6b6c9d40d793e5b94aec12d14f3c9f59f18e9
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-08-17 12:58:54 -0300

    Added buttons and putted it in to a frame

--- board.py
+++ board.py
@@ -1,6 +1,11 @@
-from PySide6.QtWidgets import QPushButton, QGridLayout
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QPushButton, QGridLayout, QFrame
+from PySide6.QtCore import Qt, Slot
 from styles import MEDIUM_FONT_SIZE
+
+
+class BoardFrame(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class Button(QPushButton):
@@ -13,13 +18,14 @@ class Button(QPushButton):
         self.setFocusPolicy(Qt.FocusPolicy.NoFocus)
 
     def configButtonStyle(self):
+        self.setStyleSheet("background:grey;height:100;width:10;")
         font = self.font()
         font.setPixelSize(MEDIUM_FONT_SIZE)
         self.setFont(font)
 
 
 class ButtonGridBoard(QGridLayout):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, board: QFrame, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self._gameBoardLayout = [
@@ -28,9 +34,30 @@ class ButtonGridBoard(QGridLayout):
             [" ", " ", " "],
         ]
 
+        self.frame = board
+        self.frame.setStyleSheet(
+            "background-color: lightgray; border: 1px solid black;"
+        )
+        self.frame.setLayout(self)
+
         self._createBoard
 
     def _createBoard(self):
         for row, text in enumerate(self._gameBoardLayout):
             for column, buttonText in enumerate(text):
                 button = Button(buttonText)
+
+                self.addWidget(button, row, column)
+
+                buttonSlot = self._makeButtonSlot(self._registerPlayerMove)
+                button.clicked.connect(buttonSlot)
+
+    def _makeButtonSlot(self, method, *args, **kwargs):
+        @Slot(bool)
+        def realSlot():
+            method(*args, **kwargs)
+
+        return realSlot
+
+    def _registerPlayerMove(self):
+        raise NotImplementedError
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from styles import setupTheme
 from player_names import playerLabels, playerSimbol
 from main_window import MainWindow
 from tittle import tittleLabel
+from board import ButtonGridBoard, BoardFrame
 
 if __name__ == "__main__":
     # Creating the app
@@ -29,6 +30,15 @@ if __name__ == "__main__":
     window.vLayout.addLayout(playerLabel)
 
     playerLabel._insertLabelOnScreen()
+
+    BoardInstance = BoardFrame()
+
+    board = ButtonGridBoard(board=BoardInstance)
+    # window.vLayout.addLayout(board)
+
+    window.vLayout.addWidget(BoardInstance)
+
+    board._createBoard()
 
     # Remove the permission to resize the window
     window.AdjustFixedSize()
